fe3c9860d9c7efd4a4349e61e525df3c21cdd2d6
kirill <kirill-esau@outlook.de>
2022-06-06 09:21:44 +0200

    Erweiterung um eigene Direktiven: - Zoom eines Bildes - Verzögerung beim Darstellen der Ratings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,8 @@ import {EditBookComponent} from './edit-book/edit-book.component';
 import {registerLocaleData} from "@angular/common";
 import localeDe from '@angular/common/locales/de';
 import { IsbnPipe } from './shared/isbn.pipe';
+import { ZoomDirective } from './shared/zoom.directive';
+import { DelayDirective } from './shared/delay.directive';
 
 @NgModule({
   declarations: [
@@ -32,7 +34,9 @@ import { IsbnPipe } from './shared/isbn.pipe';
     CreateBookComponent,
     FormMessagesComponent,
     EditBookComponent,
-    IsbnPipe
+    IsbnPipe,
+    ZoomDirective,
+    DelayDirective
   ],
   imports: [
     HttpClientModule,
